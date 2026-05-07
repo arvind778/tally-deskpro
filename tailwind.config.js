efb75be1f102953dff9d9abd0bcd7094b75054f9
arvind kumar <91278995+arvind778@users.noreply.github.com>
2026-05-07 07:21:41 +0530

    Add TypeScript support to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
